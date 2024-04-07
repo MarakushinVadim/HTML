4675c94eabcd19436c6c43e6936376b6f1a2e01d
Marakushin Vadim <marakushin.vadim@gmail.com>
2024-04-07 19:55:16 +0300

    add data to index.html add main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-<!doctype html>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "LocalHost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def __get_html_content(self):
+        return """
+        <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -408,3 +417,28 @@
 </script>
 </body>
 </html>
+        """
+
+    def do_GET(self):
+        page_content = self.__get_html_content()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+
+
+if __name__=="__main__":
+
+    webServer = HTTPServer((hostName,serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
